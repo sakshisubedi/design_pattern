a6dadbd96cbd34a18b92846f42643b92d39999b7
sakshi26-97 <sakshisubedi@gmail.com>
2022-01-29 13:56:46 +0530

    Creational:- Prototype Pattern using employee example

--- src/com/creational/prototype/Employee.java
+++ src/com/creational/prototype/Employee.java
@@ -1,2 +1,60 @@
-package com.creational.prototype;public class Employee {
+package com.creational.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee implements Cloneable {
+    private List<String> empList;
+
+    public Employee() {
+        empList = new ArrayList<>();
+    }
+
+    public Employee(List<String> empList) {
+        this.empList = empList;
+    }
+
+    // from DB
+    public void loadData() {
+        //read all employees from database and put into the list
+        empList.add("Sunil");
+        empList.add("Twisha");
+        empList.add("Sakshi");
+    }
+
+    public List<String> getEmpList() {
+        return empList;
+    }
+
+    public void remove(int index) {
+        if(index>=0 && index<empList.size()) {
+            empList.remove(index);
+        } else {
+            System.out.println("Index out of bound");
+        }
+    }
+
+    public void add(String emp) {
+        empList.add(emp);
+    }
+
+    // Return type can also be Employee instead of object
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // deep copy
+        List<String> cloneEmp = new ArrayList<>();
+        for(String emp: this.getEmpList()) {
+            cloneEmp.add(emp);
+        }
+        return new Employee(cloneEmp);
+    }
+
+    @Override
+    public String toString() {
+        String employee = "";
+        for(String emp: this.getEmpList()) {
+            employee += emp + " ";
+        }
+        return employee;
+    }
 }
--- src/com/creational/prototype/PrototypePattern.java
+++ src/com/creational/prototype/PrototypePattern.java
@@ -1,2 +1,22 @@
-package com.creational.prototype;public class PrototypePattern {
+package com.creational.prototype;
+
+public class PrototypePattern {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Employee emp1 = new Employee();
+        emp1.loadData();
+
+        System.out.println("Employee 1 " + emp1.toString());
+
+        Employee emp2 = (Employee) emp1.clone();
+        System.out.println("Employee 2 " + emp2.toString());
+        emp2.remove(2);
+
+        Employee emp3 = (Employee) emp1.clone();
+        System.out.println("Employee 3 " + emp3.toString());
+        emp3.add("XYZ");
+
+        System.out.println("Employee 1 " + emp1.toString());
+        System.out.println("Employee 2 " + emp2.toString());
+        System.out.println("Employee 3 " + emp3.toString());
+    }
 }

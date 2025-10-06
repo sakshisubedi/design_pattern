253d9170b34ec34e1e687b400c445494793439d5
Sakshi Subedi <35215747+sakshisubedi@users.noreply.github.com>
2025-10-05 17:27:11 -0700

    Update Terrorist.java

--- src/com/structural/flyweight/counterStrike/Terrorist.java
+++ src/com/structural/flyweight/counterStrike/Terrorist.java
@@ -1,7 +1,8 @@
 package com.structural.flyweight.counterStrike;
 
 /**
- * The Flyweight class contains the portion of the original object’s state that can be shared between multiple objects. The same flyweight object can be used in many contexts. The state stored inside a flyweight is called intrinsic. The state passed to the flyweight’s methods is called extrinsic.
+ * The Flyweight class contains the portion of the original object’s state that can be shared between multiple objects. 
+ The same flyweight object can be used in many contexts. The state stored inside a flyweight is called intrinsic. The state passed to the flyweight’s methods is called extrinsic.
  */
 // Terrorist must have weapon and mission
 public class Terrorist implements Player {
